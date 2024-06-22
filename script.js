f43463546a1432b5888ab8deb86116e08735ded7
wcoutinhoo <164575561+wcoutinhoo@users.noreply.github.com>
2024-06-22 20:36:11 -0300

    Update script.js
    agora com a requisição da API do GitHub e atualização no carrosel das imagens

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
-let slideIndex = 0;
-showSlides();
-function showSlides() {
-  let slides = document.querySelectorAll('.slide');
-  if (slideIndex >= slides.length) {
-    slideIndex = 0;
-  } else if (slideIndex < 0) {
-    slideIndex = slides.length - 1;
-  }
-  
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.opacity = 0;
-  }
-slides[slideIndex].style.opacity = 1;
-  slides[slideIndex].classList.add('active');
+document.addEventListener('DOMContentLoaded', function() {
+    const repositoriosContainer = document.getElementById('listarepositorios');
+
+    fetch('https://api.github.com/users/wcoutinhoo/repos')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao tentar conexão com o GitHub');
+            }
+            return response.json();
+        })
+        .then(data => {
+        data.forEach(repo => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            const html = `
+                    <h4>${repo.name}</h4>
+                    <p>${repo.description || 'Sem descrição'}</p>
+                    <a href="${repo.html_url}" target="_blank">Ver no GitHub</a>
+                `;
+            card.innerHTML = html;
+            repositoriosContainer.appendChild(card);
+        });
+    })
+    .catch(error => console.error('Erro:', error));
+});
+
+const botaoanterior = document.getElementById('botaoanterior');
+const botaoproximo = document.getElementById('botaoproximo');
+const carouselContainer = document.querySelector('.cardcarrosel');
+const slides = document.querySelectorAll('.slide');
+
+let index = 0;
+
+botaoproximo.addEventListener('click', () => {
+    index++;
+    if (index >= slides.length) {
+        index = 0;
+    }
+    carrosel();
+});
+
+botaoanterior.addEventListener('click', () => {
+    index--;
+    if (index < 0) {
+        index = slides.length - 1;
+    }
+    carrosel();
+});
+
+function carrosel() {
+    const slideWidth = slides[0].clientWidth;
+    carouselContainer.style.transform = `translateX(${-index * slideWidth}px)`;
 }
-document.getElementById('botaoanterior').addEventListener('click', () => {
-  slideIndex--;
-  showSlides();
-});
-document.getElementById('botaoproximo').addEventListener('click', () => {
-  slideIndex++;
-  showSlides();
-});
